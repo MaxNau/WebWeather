7a98840430998eaebc189353301e13ce84bb8743
governor <maxnaumovs@gmail.com>
2017-12-15 11:55:11 +0200

    Moved tab-control to the separate javascript file. Added auto-initialization of the tab-control. To initialize the tab-control user needs: include tabControl javascript file add a "div" tag with class "tab-control" which must hold one "ul" tag wit some list items inside. List items must have "a" tag. All needed id's and classes will be added dynamically if the structure is respected.

---
+++ WebWeather/Scripts/AppScripts/tabControl.js
@@ -0,0 +1,63 @@
+﻿var tabControl = {
+    initialize: function () {
+        var lastClicked;
+        var lastFragment;
+
+        var tab = document.getElementsByClassName("tab-control");
+        var ul = tab[0].getElementsByTagName("ul");
+        var items = ul[0].getElementsByTagName("li");
+        var arr = Array.from(items);
+
+        for (i = 0; i < arr.length; i++){
+            arr[i].id = i + 1;
+            var link = arr[i].getElementsByTagName("a");
+            link[0].href = "#fragment-x" + (i + 1);
+        }
+
+        doFistTabActive();
+
+       
+
+        var onTabClick = function callback(e) {
+            var e = window.e || e;
+
+            if (e.target.tagName !== 'LI')
+                return;
+
+            e.target.classList.add("tab-active");
+            var fragment = document.getElementById("f-" + e.target.id);
+
+            fragment.classList.add("fragment-active");
+
+            if (lastClicked) {
+                if (lastClicked.id != e.target.id) {
+                    lastClicked.classList.remove("tab-active");
+                    lastFragment.classList.remove("fragment-active");
+                    lastClicked = e.target;
+                    lastFragment = fragment;
+                }
+            }
+            else {
+                lastFragment = fragment;
+                lastClicked = e.target;
+            }
+        }
+
+        subscribeToTabClicks();
+
+        function subscribeToTabClicks() {
+            if (document.addEventListener)
+                document.addEventListener('click', onTabClick, false);
+            else
+                document.attachEvent('onclick', onTabClick);
+        }
+
+        function doFistTabActive() {
+            arr[0].classList.add("tab-active");
+            var fragment = document.getElementById("f-" + arr[0].id);
+            fragment.classList.add("fragment-active");
+            lastClicked = arr[0];
+            lastFragment = fragment;
+        }
+    }
+}
